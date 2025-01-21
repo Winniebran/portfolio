ab791a9dd9fc4bed0887042b0f09d31a80db1414
Winnie Brandão <106097591+Winniebran@users.noreply.github.com>
2025-01-21 11:51:25 -0300

    Update userData.ts

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -2,6 +2,6 @@ export const userData = {
   nameUser: "Winnie Brandão",
   githubUser: "Winniebran",
   linkedinUser: "winnie-brandao",
-  whatsappNumber: "993717710",
+  whatsappNumber: "71993717710",
   emailUser: "winnie_brandao@yahoo.com.br",
 };
